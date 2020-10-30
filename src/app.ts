230f97651892f45c67e98c556316b146aa670b4d
muhammedfahis <fahisccc3@gmail.com>
2020-10-30 11:50:37 +0530

    second commit

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import express,{Application,request,response,NextFunction} from 'express';
+import express,{Application,Request,Response,NextFunction} from 'express';
 
 
 const app: Application = express();
 
 const Add = (a:number , b:number):number =>a+b;
 
-app.get('/',(req ,res)=>{
+app.get('/',(req:Request ,res:Response)=>{
     console.log(Add(5,8));
     
     res.send('welcome to typescript');
